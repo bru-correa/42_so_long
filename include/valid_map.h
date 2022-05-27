895999b6d9f8726d867b9d0b5ddb658a6ae3f806
bru-correa <bruuh.cor@gmail.com>
2022-05-28 00:48:38 +0200

    refactor: fix tabs in valid_map header file

--- include/valid_map.h
+++ include/valid_map.h
@@ -6,7 +6,7 @@
 /*   By: bcorrea- <bruuh.cor@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 21:39:04 by bcorrea-          #+#    #+#             */
-/*   Updated: 2022/05/17 21:41:05 by bcorrea-         ###   ########.fr       */
+/*   Updated: 2022/05/28 00:47:54 by bcorrea-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 
 # include "so_long.h"
 
-int			is_map_valid(char **map, t_vector2d map_size);
-int			is_map_chars_valid(char **map, t_vector2d map_size);
-int			is_map_objs_count_valid(char **map, t_vector2d map_size);
-int			is_map_walled(char **map, t_vector2d map_size);
+int	is_map_valid(char **map, t_vector2d map_size);
+int	is_map_chars_valid(char **map, t_vector2d map_size);
+int	is_map_objs_count_valid(char **map, t_vector2d map_size);
+int	is_map_walled(char **map, t_vector2d map_size);
 
 #endif
