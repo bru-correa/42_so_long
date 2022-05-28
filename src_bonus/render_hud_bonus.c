104a097e34a5b4bb7332dbfee80cfb1f8a9ba013
bru-correa <bruuh.cor@gmail.com>
2022-05-28 23:02:55 +0200

    refactor: remove comments from render_hud

--- src_bonus/render_hud_bonus.c
+++ src_bonus/render_hud_bonus.c
@@ -6,13 +6,11 @@
 /*   By: bcorrea- <bruuh.cor@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/28 22:48:01 by bcorrea-          #+#    #+#             */
-/*   Updated: 2022/05/28 22:58:16 by bcorrea-         ###   ########.fr       */
+/*   Updated: 2022/05/28 23:02:40 by bcorrea-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long_bonus.h"
-
-// static char	*get_score_str(int player_steps);
 
 void	render_hud(t_game *game)
 {
@@ -24,11 +22,3 @@ void	render_hud(t_game *game)
 	mlx_string_put(game->mlx_ptr, game->win_ptr, 42, 28, 0xf8c820, score_str);
 	free(score_str);
 }
-
-// static char	*get_score_str(int player_steps)
-// {
-// 	char	*score_str;
-
-// 	score_str = ft_itoa(player_steps);
-// 	return (score_str);
-// }
